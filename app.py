1462467637f8213ab54b218cb2db795cd6ceb15c
AbdiBillow <114852130+AbdiBillow@users.noreply.github.com>
2025-01-31 11:08:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ if uploaded_file:
 
    
 
-            # Split Data
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+ # Split Data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
             # Train Model Button
             if st.button("Train Model"):
